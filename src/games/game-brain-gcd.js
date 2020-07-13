f788baf1fda25250dd3b954d0ecac290729a5e02
olya belykh <belykh.o.v@gmail.com>
2020-07-13 22:28:55 +0300

    refactor - naming

--- src/games/game-brain-gcd.js
+++ src/games/game-brain-gcd.js
@@ -4,12 +4,12 @@ export const ANNOUNCEMENT = 'Find the greatest common divisor of given numbers.'
 const NUMBER_MIN = 1;
 const NUMBER_MAX = 100;
 
-const gcd = (a, b) => {
+const calculateGcd = (a, b) => {
   if (b === 0) {
     return a;
   }
 
-  return gcd(b, a % b);
+  return calculateGcd(b, a % b);
 };
 
 const gameBrainGcd = () => {
@@ -17,7 +17,7 @@ const gameBrainGcd = () => {
   const b = generateRandomNumber(NUMBER_MIN, NUMBER_MAX);
 
   const question = `${a} ${b}`;
-  const answer = String(gcd(a, b));
+  const answer = String(calculateGcd(a, b));
 
   return { question, answer };
 };
